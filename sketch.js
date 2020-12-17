fc4586d50fc9d20e6d01f5147fe34a406d4a0724
PriyalShrimali <68710858+PriyalShrimali@users.noreply.github.com>
2020-12-17 22:52:50 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,11 @@
+var background;
+var ball;
+var insiders;
+var outsiders;
+
+function preload(){
+background = loadImage("/images/background.jpg");
+ball = loadImage("/images/ball.png");
+insiders = loadImage("/images/insiders.png");
+outsiders = loadImage("/images/outsiders.png");  
+}
